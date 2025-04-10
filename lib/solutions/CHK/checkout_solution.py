f79bd4e8a728e77675b93de8363c2928c88dc9f9
sbx_user1051 <sbx_user1051@169.254.6.65>
2025-04-10 12:07:13 +0000

    Thu Apr 10 12:07:13 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -16,15 +16,24 @@ class CheckoutSolution:
 
         #calculate cost of SKU A items, 5A > 3A > individual
         a = items['A']
-        totalCost += (a//3)*130 + (a%3)*50
+        totalCost += (a//5) * 200
+        a%=5
+        totalCost += (a//3) * 130
+        totalCost += (a%3)* 50
+
         #calculate cost of SKU B items, 30 each and 2 for 45
         b = items['B']
-        totalCost += (b//2)*45 + (b%2)*30
+        e = items['E']
+        freeB =  e//2
+        
 
         totalCost += items['C']*20
 
         totalCost += items['D'] * 15
 
+        totalCost += items['E'] * 40
+
         return totalCost
+
 
 
