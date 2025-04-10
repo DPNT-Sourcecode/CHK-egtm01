20c37feeb44242690e4c87fc8fdd33eb75dd26a0
sbx_user1051 <sbx_user1051@169.254.6.65>
2025-04-10 11:46:52 +0000

    Thu Apr 10 11:46:52 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,8 +3,19 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        if not isinstance(skus, str):
-            return -1
         
-        items = {'A':0,'B':0,'C':0,''}
- 
+        #initalise the counts of each item
+        items = {'A':0,'B':0,'C':0,'D':0}
+        #go through each item iteratively and increment
+        for item in skus:
+            if item not in items:
+                return -1
+            items[item]+=1
+
+        totalCost = 0
+
+        #calculate cost of SKU A items, 50 each and 3 for 130
+        a = items['A']
+        total += (a//3)*130 + (a%3)*50
+
+        b
