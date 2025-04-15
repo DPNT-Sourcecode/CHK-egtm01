f56cb286d73ff6c25f3a663b04f49cfc18f91be2
sbx_user1051 <sbx_user1051@169.254.4.193>
2025-04-15 10:27:15 +0000

    Tue Apr 15 10:27:15 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -40,10 +40,11 @@ class CheckoutSolution:
         totalCost += items['E'] * 40
 
         f = items['F']
-        remaining =  f%2
-        totalCost += (f*10)-(remaining*10)
+        paidFor =  f - (f//3)
+        totalCost += paidFor * 10
 
         return totalCost
+
 
 
 
