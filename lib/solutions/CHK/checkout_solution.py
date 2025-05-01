b8091e5bf299815ffbf6402bc9ae51ee79c451a2
sbx_user1051 <sbx_user1051@169.254.86.61>
2025-05-01 21:26:39 +0000

    Thu May 01 21:26:39 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -113,7 +113,7 @@ class CheckoutSolution:
         groupPrices = {'S':20, 'T':20,'X':17, 'Y':20, 'Z':21}
         groupItems = []
 
-        #append all items
+        #append all items 
         for item in groupPrices:
             for i in range(items[item]):
                 groupItems.append(groupPrices[item])
@@ -123,5 +123,6 @@ class CheckoutSolution:
         groupItems.sort(reverse=True)
 
         return totalCost
+
 
 
